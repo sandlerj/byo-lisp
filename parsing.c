4a8872ad058f38c1f28f38532b5fcb988a24fd42
Joanna Jackson-Sandler <jjackson-sandler@aurora.tech>
2026-02-16 18:09:23 +0000

    use s-expressions

--- parsing.c
+++ parsing.c
@@ -147,6 +147,109 @@ void lval_print(lval* v) {
 	}
 }
 
+lval* lval_eval(lval* v);
+lval* lval_take(lval* v, int i);
+lval* lval_pop(lval* v, int i);
+lval* lval_take(lval* v, int i);
+lval* builtin_op(lval* a, char* op);
+
+lval* lval_eval_sexpr(lval* v) {
+	/* Eval any children */
+	for (int i = 0; i < v->count; i++) {
+		v->cell[i] = lval_eval(v->cell[i]);
+	}
+
+	/* Check all for errors */
+	for (int i = 0; i < v->count; i++) {
+		if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+	}
+
+	if (v->count == 0) { return v; }
+
+	if (v->count == 1) { return lval_take(v, 0); }
+
+	/* Ensure first element is a symbol */
+	lval* f = lval_pop(v, 0);
+	if (f->type != LVAL_SYM) {
+		lval_del(f); lval_del(v);
+		return lval_err("S-expression does not start with symbol!");
+	}
+
+	/* Call with built in op */
+	lval* result = builtin_op(v, f->sym);
+	lval_del(f);
+	return result;
+}
+
+lval* lval_eval(lval* v) {
+	/* Evaluate s-expresion */
+	if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+
+	return v;
+}
+
+lval* lval_pop(lval* v, int i) {
+	/* find the item at i */
+	lval* x = v->cell[i];
+
+	/* Shift memory after item at i over the top */
+	memmove(&v->cell[i], &v->cell[i+1],
+			sizeof(lval*) * (v->count-i-1));
+
+	v->count--;
+
+	/* reallocate to small size */
+	v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+
+	return x;
+}
+
+/* Take lval at i and delete the rest */
+lval* lval_take(lval* v, int i) {
+	lval* x = lval_pop(v, i);
+	lval_del(v);
+	return x;
+}
+
+lval* builtin_op(lval* a, char* op) {
+	for (int i = 0; i < a->count; i++) {
+		if (a->cell[i]->type != LVAL_NUM) {
+			lval_del(a);
+			return lval_err("Cannot operate on non-numbers");
+		}
+	}
+
+	/* pop first element */
+	lval* x = lval_pop(a, 0);
+
+	/* if no args and sub then do unary negation */
+	if ((strcmp(op, "-") == 0) && a->count == 0 ) {
+		x->num = -x->num;
+	}
+
+	while (a->count > 0) {
+		lval* y = lval_pop(a, 0);
+
+		if (strcmp(op, "+") == 0 ) { x->num += y->num; }
+		if (strcmp(op, "-") == 0 ) { x->num -= y->num; }
+		if (strcmp(op, "*") == 0 ) { x->num *= y->num; }
+		if (strcmp(op, "/") == 0 ) { 
+			if (y->num == 0) {
+				lval_del(x); lval_del(y);
+				x = lval_err("Division by Zero!"); break;
+			}
+			x->num /= y->num;
+		}
+		if (strcmp(op, "min") == 0 ) { x->num = fminl(x->num, y->num); }
+		if (strcmp(op, "max") == 0 ) { x->num = fmaxl(x->num, y->num); }
+		
+		lval_del(y);
+	}
+
+	lval_del(a);
+	return x;
+}
+
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 int main(int argc, char** argv) {
 	mpc_parser_t* Number 	= mpc_new("number");
@@ -178,7 +281,7 @@ int main(int argc, char** argv) {
 		/* try parse */
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Lispy, &r)) {
-			lval* x = lval_read(r.output);
+			lval* x = lval_eval(lval_read(r.output));
 			lval_println(x);
 			lval_del(x);
 			mpc_ast_delete(r.output);
